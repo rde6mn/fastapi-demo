804f42c5ca600563de0d07d4b00e78a3a1c8687c
Mohini Gupta <rde6mn@virginia.edu>
2024-03-31 05:08:47 +0000

    code for getting data from database

--- app/main.py
+++ app/main.py
@@ -25,10 +25,9 @@ DB = "rde6mn"
 # The URL for this API has a /docs endpoint that lets you see and test
 # your various endpoints/methods.
 
-@app.get("/")  # zone apex
-def zone_apex():
-    return {"Hello": "Hello API", "album_endpoint":"/albums","static_endpoint":"/static"}
-
+#@app.get("/")  # zone apex
+#def zone_apex():
+#    return {"Hello": "Hello API", "album_endpoint":"/albums","static_endpoint":"/static"}
 
 
 @app.get("/albums")
@@ -40,14 +39,14 @@ def get_all_albums():
     db.close()
     return results
 
-@app.get("/albums/{id}")
-def get_one_album(id):
-    db = MySQLdb.connect(host=HOST, user=USER, passwd=PASS, db=DB)
-    c = db.cursor(MySQLdb.cursors.DictCursor)
-    c.execute("SELECT * FROM albums WHERE id=" + id)
-    results = c.fetchall()
-    db.close()
-    return results
+#@app.get("/albums/{id}")
+#def get_one_album(id):
+#    db = MySQLdb.connect(host=HOST, user=USER, passwd=PASS, db=DB)
+#    c = db.cursor(MySQLdb.cursors.DictCursor)
+#    c.execute("SELECT * FROM albums WHERE id=" + id)
+#    results = c.fetchall()
+#    db.close()
+#    return results
 
 
     
